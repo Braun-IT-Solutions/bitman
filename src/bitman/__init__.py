1cf45943960fd01b88b220bba6ed6dc7a93be33f
Daniel Spindler <daniel.spindler@braun-its.de>
2025-04-15 11:25:25 +0200

    Updated formatting

--- src/bitman/__init__.py
+++ src/bitman/__init__.py
@@ -1,4 +1,5 @@
 from argparse import Namespace
+from rich.console import Console
 from bitman.config.system_config import SystemConfig
 from bitman.pacman import Pacman
 from bitman.sync import Sync
@@ -9,18 +10,19 @@ class Bitman:
         self._system_config = SystemConfig()
         self._pacman = Pacman()
         self._sync = Sync(self._system_config, self._pacman)
+        self._console = Console()
 
     def sync(self, args: Namespace):
         """Processes bitman sync command"""
         if args.status:
             status = self._sync.status()
-            print('Additional:')
-            print(*['\t' + line for line in status.additional], sep='\n')
+            self._console.print('Additional', style='bold yellow')
+            self._console.print(*['[bold]·[/bold] ' + line for line in status.additional], sep='\n')
 
-            print('\nMissing from Arch Repository:')
-            print(*['\t' + line for line in status.missing_arch], sep='\n')
-
-            print('\nMissing from AUR:')
-            print(*['\t' + line for line in status.missing_aur], sep='\n')
+            self._console.print('\nMissing', style='bold red')
+            self._console.print(
+                *['[bold]·[/bold] ' + line for line in status.missing_arch], sep='\n')
+            self._console.print(*['[bold]·[/bold] ' + line +
+                                '(AUR)' for line in status.missing_aur], sep='\n')
         else:
             print('Not implemented')
